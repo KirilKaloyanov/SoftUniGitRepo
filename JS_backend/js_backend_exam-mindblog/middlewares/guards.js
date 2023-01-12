1fee2515bd44b8416819de429489e5718fe206c6
Kiril <kaloyanov.kiril@gmail.com>
2023-01-12 15:40:48 +0200

    Bonus

---
+++ JS_backend/js_backend_exam-mindblog/middlewares/guards.js
@@ -0,0 +1,47 @@
+const { getItem } = require("../services/itemService");
+
+function hasUser(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/error");
+  }
+}
+
+function isGuest(req, res, next) {
+  if (req.user) {
+    res.redirect("/error"); //TODO: check assignment for correct redirect
+  } else {
+    next();
+  }
+}
+
+async function midUser(req, res, next) {
+  if (req.user) {
+    try {
+      const item = await getItem(req.params.id);
+      // console.log(req.user);
+
+      if (item.owner.toString() === req.user._id) {
+        req.user.isOwner = true;
+      } else {
+        req.user.isVisitor = true;
+        if (
+          item.subscribeList.map((w) => w.toString()).indexOf(req.user._id) >= 0
+        )
+          req.user.subscribed = true;
+      }
+    } catch (err) {
+      next();
+    }
+    next();
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  hasUser,
+  isGuest,
+  midUser,
+};
